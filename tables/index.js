44a5033ec5ec4823cb4fdabb66683c5903660e01
Esteban Indiveri <esteban.indiveri@gmail.com>
2020-12-12 11:53:27 -0300

    add teste tables

--- tables/index.js
+++ tables/index.js
@@ -10,21 +10,27 @@ async function tableForm() {
 
     try {
         (await driver).get('https://rori4.github.io/selenium-practice/#/pages/tables/smart-table');
-        let rows = await driver.findElements(By.xpath("//tbody/tr"));
-        for (const row of rows) {
-            let rowData = await row.getText();
-            let user=rowData.split(/\n/);
-            results.push({
-                id:user[0],
-                firstName:user[1],
-                lastName:user[2],
-                username:use[3],
-                email:user[4],
-                age:user[5],
 
-            })
-            debugger;
+        for (let i = 1; i < 6; i++) {
+            let rows = await driver.findElements(By.xpath("//tbody/tr"));
+            for (const row of rows) {
+                let rowData = await row.getText();
+                let user=rowData.split(/\n/);
+                results.push({
+                    id:user[0],
+                    firstName:user[1],
+                    lastName:user[2],
+                    username:user[3],
+                    email:user[4],
+                    age:user[5],
+
+                });
+            }
+            await driver.findElement(By.css('.page-link-next')).click();
+
         }
+
+        
         console.table(results);
 
     } catch (error) {
